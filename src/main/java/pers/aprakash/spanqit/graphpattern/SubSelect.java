ab48b159a13fab7cc4e9b602069f221e97b0d3ed
Ankit Prakash <ankit.n.prakash@gmail.com>
2014-07-09 21:38:12 -0700

    removing eclipse files, beginning changes to subselect

--- src/main/java/pers/aprakash/spanqit/graphpattern/SubSelect.java
+++ src/main/java/pers/aprakash/spanqit/graphpattern/SubSelect.java
@@ -1,31 +1,39 @@
 package pers.aprakash.spanqit.graphpattern;
 
 import pers.aprakash.spanqit.constraint.Expression;
+import pers.aprakash.spanqit.core.GroupBy;
+import pers.aprakash.spanqit.core.Having;
+import pers.aprakash.spanqit.core.OrderBy;
 import pers.aprakash.spanqit.core.SelectQuery;
 
-public class SubSelect extends SelectQuery implements GraphPattern {
-	@Override
+public class SubSelect implements GraphPattern {
+	protected static final String LIMIT = "LIMIT";
+	protected static final String OFFSET = "OFFSET";
+
+	protected QueryPattern where = new QueryPattern();  // ? for DESCRIBE, required otherwise
+	protected GroupBy groupBy; // ?
+	protected OrderBy orderBy; // ?
+	protected Having having; // ?
+	protected int limit = -1, offset = -1, varCount = -1;  // ?
+	
 	public GraphPattern and(GraphPattern... patterns) {
 		where.and(patterns);
 		
 		return this;
 	}
 
-	@Override
 	public GraphPattern union(GraphPattern... patterns) {
 		where.union(patterns);
 		
 		return this;
 	}
 
-	@Override
 	public GraphPattern optional(boolean isOptional) {
 		where.optional(isOptional);
 		
 		return this;
 	}
 
-	@Override
 	public GraphPattern filter(Expression constraint) {
 		where.filter(constraint);
 		
@@ -34,9 +42,12 @@ public class SubSelect extends SelectQuery implements GraphPattern {
 	
 	@Override
 	public String getQueryString() {
-		setBase(null);
-		prefixes = null;
 		
 		return super.getQueryString();
+	}
+
+	@Override
+	public String getPrettyQueryString(int indent) {
+		return getQueryString();
 	}
 }
